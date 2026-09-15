0098c3b41b3daba10c9e51db425cd4676abc1258
unknown <kunalvagh45@gmail.com>
2026-09-15 11:11:04 +0530

    Fix Vercel Flutter build

---
+++ vercel-build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing Flutter 3.29.0..."
+
+git clone --depth 1 https://github.com/flutter/flutter.git /tmp/flutter
+
+cd /tmp/flutter
+
+git fetch --depth 1 origin 35c388afb57ef061d06a39b537336c87e0e3d1b1
+
+git checkout 35c388afb57ef061d06a39b537336c87e0e3d1b1
+
+echo "Flutter version:"
+/tmp/flutter/bin/flutter --version
+
+/tmp/flutter/bin/flutter config --enable-web
+
+cd "$VERCEL_PROJECT_DIR"
+
+echo "Getting dependencies..."
+/tmp/flutter/bin/flutter pub get
+
+echo "Building Flutter Web..."
+/tmp/flutter/bin/flutter build web --release
+
+echo "Flutter Web build completed!"
