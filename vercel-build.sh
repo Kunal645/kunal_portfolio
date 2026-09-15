4776bf322645f8d8db4f1177ac28485874892ac8
unknown <kunalvagh45@gmail.com>
2026-09-15 11:13:40 +0530

    Allow Flutter build on Vercel

--- vercel-build.sh
+++ vercel-build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+export FLUTTER_ALLOW_ROOT=1
+
 echo "Installing Flutter 3.29.0..."
 
 git clone --depth 1 https://github.com/flutter/flutter.git /tmp/flutter
@@ -13,6 +15,7 @@ git fetch --depth 1 origin 35c388afb57ef061d06a39b537336c87e0e3d1b1
 git checkout 35c388afb57ef061d06a39b537336c87e0e3d1b1
 
 echo "Flutter version:"
+
 /tmp/flutter/bin/flutter --version
 
 /tmp/flutter/bin/flutter config --enable-web
@@ -20,9 +23,13 @@ echo "Flutter version:"
 cd "$VERCEL_PROJECT_DIR"
 
 echo "Getting dependencies..."
+
+export PUB_CACHE=/tmp/pub-cache
+
 /tmp/flutter/bin/flutter pub get
 
 echo "Building Flutter Web..."
+
 /tmp/flutter/bin/flutter build web --release
 
 echo "Flutter Web build completed!"
